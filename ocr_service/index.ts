c0b93f7450c11c511993d1657fa2d2f981657907
elaad24 <elaad2408@gmail.com>
2024-10-25 17:25:07 +0300

    added ocr with teserct.js in ts node ts

---
+++ ocr_service/index.ts
@@ -0,0 +1,38 @@
+const tesseract = require("tesseract.js");
+const fs = require("fs");
+const path = require("path");
+
+const convert_img_to_txt = async () => {
+  const dir_files_to_convert = "../imagesFolder/preSort";
+  try {
+    fs.readdir(dir_files_to_convert, (err, files) => {
+      if (err) {
+        return console.log("Unable to scan directory:", err);
+      }
+      files.forEach(async (file) => {
+        const filePath = path.join(dir_files_to_convert, file);
+        const outputPath = `../classification_service/text_files/${file}.txt`;
+
+        const result = await tesseract.recognize(filePath, "eng", {
+          logger: (e) => console.log(e.status, e.progress),
+        });
+        console.log(result.data.text);
+        saveTextToFile(result.data.text, outputPath);
+      });
+    });
+  } catch (err) {
+    console.error("Error processing image to text:", err);
+  }
+};
+
+const saveTextToFile = (text, outputPath) => {
+  fs.appendFile(outputPath, text, (err) => {
+    if (err) {
+      console.error("Error writing to file:", err);
+    } else {
+      console.log("Text successfully written as", outputPath);
+    }
+  });
+};
+
+convert_img_to_txt();
